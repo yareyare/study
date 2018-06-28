d25457cdd4234d3ae947d893c22ab405f2c4f72a
yare123 <522378972@qq.com>
2018-06-28 15:56:59 +0800

    优化

--- src/main/java/design/proxy/xmlSpringAopProxy/Advices.java
+++ src/main/java/design/proxy/xmlSpringAopProxy/Advices.java
@@ -8,30 +8,29 @@ public class Advices {
     //前置通知
     public void before(JoinPoint jp)
     {
-        System.out.println("--------------------bofore--------------------");
-        System.out.println("方法名："+jp.getSignature()+"，参数："+jp.getArgs().length+"，代理对象："+jp.getTarget());
+        System.out.println("【bofore】"+jp.getSignature().getName()+"----------------------------------------");
+        //System.out.println("方法名："+jp.getSignature()+"，参数："+jp.getArgs().length+"，代理对象："+jp.getTarget());
     }
     //后置通知
     public void after(JoinPoint jp){
-        System.out.println("--------------------after--------------------");
+        System.out.println("【after】"+jp.getSignature().getName()+"----------------------------------------");
     }
 
     //环绕通知
     public Object around(ProceedingJoinPoint pjd) throws Throwable{
-        System.out.println("--------------------环绕开始--------------------");
+        System.out.println("【around】start："+pjd.getSignature().getName()+"----------------------------------------");
         Object object=pjd.proceed();
-        System.out.println("--------------------环绕结束--------------------");
+        System.out.println("【around】end："+pjd.getSignature().getName()+"----------------------------------------");
         return object;
     }
     //异常后通知
     public void afterThrowing(JoinPoint jp,Exception exp)
     {
-        System.out.println("--------------------异常后通知，发生了异常："+exp.getMessage()+"--------------------");
+        System.out.println("【afterThrowing】"+jp.getSignature().getName()+" 异常后通知，发生了异常："+exp.getMessage()+"--------------------");
     }
     //返回结果后通知
     public void afterReturning(JoinPoint joinPoint, Object result)
     {
-        System.out.println("--------------------返回结果后通知--------------------");
-        System.out.println("结果是："+result);
+        System.out.println("【afterReturning】"+joinPoint.getSignature().getName()+"----------------------------------------result="+result);
     }
 }
